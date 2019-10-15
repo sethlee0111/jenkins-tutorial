86879d97477b9e26831224ea03d2cebcabfb12d4
Seth Sangsu Lee <sethlee@utexas.edu>
2019-10-14 22:04:58 -0500

    Update controlled_tests.py

--- controlled_tests.py
+++ controlled_tests.py
@@ -9,19 +9,3 @@ class ControlledTests(unittest.TestCase):
         computed_result = compute_highest_affinity.highest_affinity(site_list, user_list, time_list)
         expected_result = ("a.com", "b.com")
         self.assertEqual(computed_result, expected_result)
-
-    def test2(self):
-        num_lines = 10000
-        num_users = 1000
-
-        site_list = randomized_input.randomized_site_list(num_lines)
-        user_list = randomized_input.randomized_user_list(num_lines, num_users)
-        time_list = range(0, num_lines)
-
-        computed_result = compute_highest_affinity.highest_affinity(
-            site_list, user_list, time_list)
-        expected_result = ("facebook", "google")
-
-        assert computed_result == expected_result
-        print(
-            "Successfully passed {}!".format(os.path.basename(__file__).split(".")[0]))
